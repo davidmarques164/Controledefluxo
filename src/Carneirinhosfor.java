01bd7b3202acf7c2e37846473b7102cddec716e2
davidmarques164 <davidpeterpaulmarques@gmail.com>
2022-10-11 00:46:14 -0300

    go

---
+++ src/Carneirinhosfor.java
@@ -0,0 +1,8 @@
+public class Carneirinhosfor {
+    public static void main(String[] args) {
+        for ( int caneirinhos = 1; caneirinhos <=20; caneirinhos ++){
+            System.out.println("Contando carneirinhos" + caneirinhos);
+        }
+        System.out.println("Joaozinho dormiu!");
+    }
+}
---
+++ src/Numerobreak.java
@@ -0,0 +1,10 @@
+public class Numerobreak {
+    public static void main(String[] args) {
+        for(int numero = 1; numero <= 5; numero ++){
+            if (numero == 8)
+                break;
+            System.out.println(numero);
+
+        }
+    }
+}
